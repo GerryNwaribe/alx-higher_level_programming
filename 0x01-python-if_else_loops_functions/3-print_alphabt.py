265b8cb74d43ee8fffebec8a688a213b084c595b
GerryNwaribe <the.star.is.geraldine@gmail.com>
2023-10-31 14:44:34 +0100

    pycode check

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 for a in range(97, 123):
-    if a != 113 and a != 101:   
+    if a != 113 and a != 101:
         char = chr(a)
-        print("{}".format(char), end='')    
+        print("{}".format(char), end='')
+
