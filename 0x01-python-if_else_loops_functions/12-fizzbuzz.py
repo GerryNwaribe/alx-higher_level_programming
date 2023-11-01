219af2c0997a43a759f2cf1abb38a44368e8cb02
GerryNwaribe <the.star.is.geraldine@gmail.com>
2023-11-02 00:44:09 +0100

    fizzbuzz multiples

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+def fizzbuzz():
+    for a in range(1, 101):
+        if a % 3 == 0 and a % 5 == 0:
+            print("{}".format('FizzBuzz'), end=' ')
+        elif a % 3 == 0:
+            print("{}".format('Fizz'), end=' ')
+        elif a % 5 == 0:
+            print("{}".format('Buzz'), end=' ')
+        else:
+            print("{}".format(a), end=' ')
+
