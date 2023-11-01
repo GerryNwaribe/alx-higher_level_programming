50136a0270d8231bf74439ca5607c0ecf082887b
GerryNwaribe <the.star.is.geraldine@gmail.com>
2023-11-01 22:57:23 +0100

    pycode style

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,11 +3,11 @@ def uppercase(s):
     result = ""
     for char in s:
         if 'a' <= char <= 'z':
-           
+
             uppercase_char = chr(ord(char) - 32)
             result += uppercase_char
         else:
-            
+
             result += char
 
     print("{}".format(result))
