c9a03a7bf82321e52000da59566c2ee13cb6cfcf
GerryNwaribe <the.star.is.geraldine@gmail.com>
2023-11-03 22:09:50 +0100

    added full stop

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,9 +3,8 @@ if __name__ == "__main__":
     import sys
     a = len(sys.argv) - 1
     if a < 1:
-        print("{} arguments".format(a))
+        print("{} arguments.".format(a))
     else:
         print("{} {}:".format(a, a == 1 and "argument" or "arguments"))
         for b, arg in enumerate(sys.argv[1:], start=1):
             print("{}: {}".format(b, arg))
-    
