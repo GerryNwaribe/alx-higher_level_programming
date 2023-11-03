677ed937c8c2518d6bed5d8f186e10e718e03a9e
GerryNwaribe <the.star.is.geraldine@gmail.com>
2023-11-03 22:03:06 +0100

    made it run as script

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
-import sys
-a = len(sys.argv) - 1
-print("{} {}:".format(a, a == 1 and "argument" or "arguments"))
-for b, arg in enumerate(sys.argv[:1], 1):
-    print("{}: {}".format(b, arg))
+if __name__ == "__main__":
+    import sys
+    a = len(sys.argv) - 1
+    if a == 0:
+        print("{}".format('.'))
+    else:
+        print("{} {}:".format(a, a == 1 and "argument" or "arguments"))
+    for b, arg in enumerate(sys.argv[:1], 1):
+        print("{}: {}".format(b, arg))
