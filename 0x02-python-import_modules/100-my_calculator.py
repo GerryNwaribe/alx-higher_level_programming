18a47b8d628babe99ac722416543eae797d81004
GerryNwaribe <the.star.is.geraldine@gmail.com>
2023-11-04 21:59:23 +0100

    program for basic arithmetic

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    from calculator_1 import add, sub, mul, div
+    a = sys.argv[1:]
+    if len(a) != 3:
+        print("Usage: {} <a> <operator> <b>".format(sys.argv[0]))
+
+    c = int(a[0])
+    operator = a[1]
+    d = int(a[2])
+    if operator == '+':
+        result = add(c, d)
+    elif operator == '-':
+        result = sub(c, d)
+    elif operator == '*':
+        result = mul(c, d)
+    elif operator == '/':
+        result = div(c, d)
+    else:
+        print("{}".format('Unknown operator. Available operators: +, -, * and /'))
+
+print("{} {} {} = {}".format(c, operator, d, result))
+
