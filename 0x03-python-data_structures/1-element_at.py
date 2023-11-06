1bfce1829c75188f27dc75c4409e9954cca590ac
GerryNwaribe <the.star.is.geraldine@gmail.com>
2023-11-06 15:32:20 +0100

    changed second statement

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -4,7 +4,7 @@ def element_at(my_list, idx):
     for a in my_list:
         if idx < 0:
             return None
-        elif idx > b:
+        elif idx >= b:
             return None
         else:
             return my_list[idx]
