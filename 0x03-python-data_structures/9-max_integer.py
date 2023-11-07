c7bed196c954df0543589ba4fb9960bdd3831212
GerryNwaribe <the.star.is.geraldine@gmail.com>
2023-11-07 01:44:21 +0100

    changed if statement

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if not my_list or len(my_list) == 0:
+    if not my_list and len(my_list) == 0:
         return None
 
     biggest = my_list[0]
