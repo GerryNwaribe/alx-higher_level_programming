7ea02b923af913a0528aaf118b562cdf3fc3f299
GerryNwaribe <the.star.is.geraldine@gmail.com>
2023-11-07 01:38:28 +0100

    function to find max value

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if not my_list:
+        return None
+
+    biggest = my_list[0]
+
+    for a in my_list:
+        if a > biggest:
+            biggest = a
+        else:
+            return max(my_list)
