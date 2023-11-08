0e38bdb1768775a46c3bc065e831a2306c2eb2b6
GerryNwaribe <the.star.is.geraldine@gmail.com>
2023-11-08 23:08:44 +0100

    created empty set

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
+    common = set()
     for a in set_1:
         for b in set_2:
             if a in set_2:
-                return a
+                common.add(a)
+    return common
