a5fb00571e22e2857295d4e9c80cd9ad2d6c979b
GerryNwaribe <the.star.is.geraldine@gmail.com>
2023-11-20 22:06:12 +0100

    added if statement

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        print("{:d}".format(value))
-        print()
-        return True
+        if value in range(-9999, 9999):
+            print("{:d}".format(value))
+            print()
+            return True
     except (ValueError, TypeError):
         return False
