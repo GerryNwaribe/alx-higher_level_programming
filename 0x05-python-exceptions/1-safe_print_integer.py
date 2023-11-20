496898b1280e7914ccf4353b50babb0ce1b7a0ff
GerryNwaribe <the.star.is.geraldine@gmail.com>
2023-11-20 22:04:29 +0100

    fixed except and removed loop

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        if value in range(-9999, 9999):
-            print("{:d}".format(value))
-            print()
-            return True
-    except IndexError:
+        print("{:d}".format(value))
+        print()
+        return True
+    except (ValueError, TypeError):
         return False
