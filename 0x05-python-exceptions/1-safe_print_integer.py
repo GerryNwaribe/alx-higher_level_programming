8fa5a7aa72795fdf66b9d56280b8f4b26e5075ba
GerryNwaribe <the.star.is.geraldine@gmail.com>
2023-11-20 22:09:40 +0100

    converted value to integer

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        if value in range(-9999, 9999):
-            print("{:d}".format(value))
-            print()
-            return True
+        int_val = int(value)
+        print("{:d}".format(value))
+        print()
+        return True
     except (ValueError, TypeError):
         return False
