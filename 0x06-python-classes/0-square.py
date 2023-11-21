09e8aa9eb5e85041852079da442dd58eeef84bb1
GerryNwaribe <the.star.is.geraldine@gmail.com>
2023-11-21 21:43:17 +0100

    function to define square

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,19 @@
+#!/user/bin/python3
+"""Define Square class"""
+
+
+class Square:
+    """Class represents a square"""
+    def __init__(self, size=0):
+        """Initialize square of any size
+        Returns 0 if no size
+        Raises:
+            TypeError: if size is not an integer
+            ValueError: If size is less than 0
+        """
+        if not isinstance(size, int):
+            raise TypeError("Size must be an integer")
+        elif size < 0:
+            raise ValueError("Size must be >= 0")
+        else:
+            self.__size = size
