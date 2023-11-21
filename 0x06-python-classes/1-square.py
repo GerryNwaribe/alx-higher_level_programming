4b456612fd8adc13b7c38cacb167540c0d0db83f
GerryNwaribe <the.star.is.geraldine@gmail.com>
2023-11-21 22:05:55 +0100

    scrapped first attempt, defined just class square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,24 @@
+#!/user/bin/python3
+"""Define Square class
+    Attribute:
+    size(int): size of squaree
+    Returns 0 if not soecified"""
+
+
+class Square:
+    """Class represents a square"""
+    def __init__(self, size=0):
+        """Initialize square of any size
+        Attribute:
+            size(int): size of square
+        Returns 0 if no size
+        Raises:
+            TypeError: if size is not an integer
+            ValueError: If size is less than 0
+        """
+        if not isinstance(size, int):
+            raise TypeError("Size must be an integer")
+        elif size < 0:
+            raise ValueError("Size must be >= 0")
+        else:
+            self.__size = size
