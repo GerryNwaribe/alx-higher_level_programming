81bed2783b0d7d3020df063e5c97bb7fc88499e4
GerryNwaribe <the.star.is.geraldine@gmail.com>
2023-11-22 12:56:54 +0100

    documentation again

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -8,12 +8,12 @@
 class Square:
     def __init__(self, size=0):
         """Initialize square of any size
-        Attribute:
-            size(int): size of square (Private instance attribute)
-        Returns 0 if no size
-        Raises:
-            TypeError: size must be an integer
-            ValueError: size must be >= 0
+            Attribute:
+                size(int): size of square (Private instance attribute)
+            Returns 0 if no size
+            Raises:
+                TypeError: size must be an integer
+                ValueError: size must be >= 0
         """
 
         if not isinstance(size, int):
