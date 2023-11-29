a6c32f85545ccc88262dda247f6ab85161f3a166
GerryNwaribe <the.star.is.geraldine@gmail.com>
2023-11-30 00:20:59 +0100

    documentation

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
 def say_my_name(first_name, last_name=""):
+    """_summary_
+
+    Args:
+        first_name (str): first name of person
+        last_name (str, optional): last name of person. Defaults to "".
+
+    Raises:
+        TypeError: first_name must be a string
+        TypeError: last_name must be a string
+    """
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
