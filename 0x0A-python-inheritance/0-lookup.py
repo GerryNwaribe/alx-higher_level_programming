2d0202dc22edab224cae44587c1ea09776e13a0f
GerryNwaribe <the.star.is.geraldine@gmail.com>
2023-12-05 23:25:14 +0100

    fixed function

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
 def lookup(obj):
-    list = obj
-    return list
+    attributes_and_methods = []
+
+    for name in dir(obj):
+        if not callable(getattr(obj, name)) or name.startswith("__"):
+            attributes_and_methods.append(name)
+
+    return attributes_and_methods
