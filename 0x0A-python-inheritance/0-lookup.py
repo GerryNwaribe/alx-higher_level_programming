06e92510a2ac72390f157200bb15a53391bd135a
GerryNwaribe <the.star.is.geraldine@gmail.com>
2023-12-06 01:59:56 +0100

    used dir() function

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -11,34 +11,6 @@ def lookup(obj):
 
     Returns: list object
     """
-    attributes_and_methods = []
+    return dir(obj)
 
-    for name in dir(obj):
-        if not callable(getattr(obj, name)) or name.startswith("__"):
-            attributes_and_methods.append(name)
-
-    return attributes_and_methods
-
-
-def handle_int(self, value):
-    print(f"Handling integer: {value}")
-
-
-def handle_float(self, value):
-    print(f"Handling float: {value}")
-
-
-def handle_list(self, value):
-    print(f"Handling list: {value}")
-
-
-def handle_custom_class(self, value):
-    if isinstance(value, MyClass):
-        print("Handling an instance of MyClass")
-        value.one_meth()
-    else:
-        print(f"Unknown custom class: {type(value)}")
-    """case: int
-    case: float
-    case: list
-    class MyClass(): def one_meth(self): pass pass"""
+    """dir() - gets list of attributes of an object"""
