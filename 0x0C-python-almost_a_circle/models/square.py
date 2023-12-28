adbf25adb48c7e0cb6ac97fedcf8a36f69d7ff03
GerryNwaribe <the.star.is.geraldine@gmail.com>
2023-12-28 23:18:48 +0100

    adjusted return value

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -58,8 +58,7 @@ class Square(Rectangle):
         Returns:
             returns the dictionary representation of a Rectangle
         """
-        return {"id": self.id, "width": self.width,
-                "height": self.height, "x": self.x, "y": self.y}
+        return {"id": self.id, "size": self.size, "x": self.x, "y": self.y}
 
     def __str__(self):
         """overloading __str__ method
