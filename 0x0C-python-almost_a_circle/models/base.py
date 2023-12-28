6695f6cb7a52c813c521ced9924f7b2ca004860c
GerryNwaribe <the.star.is.geraldine@gmail.com>
2023-12-28 23:28:42 +0100

    json string rep

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 """define class Base"""
 
+import json
+"""import json"""
+
 
 class Base:
     """initializing class base"""
@@ -17,3 +20,17 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Return the JSON string representation of list_dictionaries.
+
+        Args:
+            list_dictionaries (list): List of dictionaries.
+
+        Returns:
+            str: JSON string representation.
+        """
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
