135d30328e18de036d7a210dd3d25b7db6cd9ede
GerryNwaribe <the.star.is.geraldine@gmail.com>
2023-12-28 23:58:42 +0100

    son string rep for list_objs

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -34,3 +34,24 @@ class Base:
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Write the JSON string representation of list_objs to a file.
+
+        Args:
+            list_objs (list): List of instances.
+
+        Returns:
+            None
+        """
+        if list_objs is None:
+            list_objs = []
+
+        class_name = cls.__name__
+        filename = f"{class_name}.json"
+        dict_list = list(map(lambda obj: obj.to_dictionary(), list_objs))
+        json_string = cls.to_json_string(dict_list)
+
+        with open(filename, 'w') as file:
+            file.write(json_string)
