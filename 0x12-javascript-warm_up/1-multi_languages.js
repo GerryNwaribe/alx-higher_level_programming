1e611923644d6ca47cf523588c131985580024ad
GerryNwaribe <the.star.is.geraldine@gmail.com>
2024-02-12 23:06:36 +0100

    printed 3 lines

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// Write a script that prints 3 lines
+const myVar = 'C is fun';
+const myVar1 = 'Python is cool';
+const myVar2 = 'JavaScript is amazing';
+
+console.log(myVar);
+console.log(myVar1);
+console.log(myVar2);
