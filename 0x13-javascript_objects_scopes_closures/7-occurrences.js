e564a6600d2cb464ec9601fbb46c9aee8d73db11
GerryNwaribe <the.star.is.geraldine@gmail.com>
2024-02-15 00:45:10 +0100

    added comment

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
 exports.nbOccurences = function (list, searchElement) {
+  // function that returns the number of occurrences in a list
   let count = 0;
   for (let i = 0; i < list.length; i++) {
     if (list[i] === searchElement) {
