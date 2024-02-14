6aaed8e9dd42f8ae9360fb7eab363e9b8d9aa736
GerryNwaribe <the.star.is.geraldine@gmail.com>
2024-02-14 23:07:09 +0100

    changed if statement again

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,12 +1,13 @@
 #!/usr/bin/node
 class Rectangle {
   constructor (w, h) {
-    if (w <= 0 || h <= 0) {
-      return {};
-    } else {
+    if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
+    } else {
+    // Create an empty object
     }
   }
 }
+
 module.exports = Rectangle;
