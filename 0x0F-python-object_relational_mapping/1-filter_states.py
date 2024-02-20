e4c6a144776abea3d6171770e08961e1463f41d3
GerryNwaribe <the.star.is.geraldine@gmail.com>
2024-02-20 20:12:19 +0100

    updated cursor execute

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -13,7 +13,8 @@ if __name__ == "__main__":
                          passwd=password, db=database_name)
     cursor = db.cursor()
 
-    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%'")
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%'" 
+                   "ORDER BY states.id ASC")
     states = cursor.fetchall()
 
     seen_states = set()
