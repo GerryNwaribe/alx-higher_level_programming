18dd8e4b7e210eee46ac181ce09a6a2c74d58d06
GerryNwaribe <the.star.is.geraldine@gmail.com>
2024-02-20 20:26:14 +0100

    documentation

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Import sys and MySQLdb."""
+import MySQLdb
+import sys
+
+"""Script that takes in an argument and displays all values in the states
+    table of hbtn_0e_0_usa where name matches the argument"""
+if __name__ == "__main__":
+    """Connect to the MySQL database and list all states."""
+    username, password, database_name, state_name_searched = sys.argv[1:5]
+
+    db = MySQLdb.connect(host="localhost", port=3306, user=username,
+                         passwd=password, db=database_name)
+    cursor = db.cursor()
+
+    """Use format to create the SQL query with the user input"""
+    query = "SELECT * FROM states "
+    "WHERE name = '{}' ORDER BY states.id ASC".format(state_name_searched)
+    cursor.execute(query)
+    states = cursor.fetchall()
+
+    for state in states:
+        print(state)
+
+    db.close()
