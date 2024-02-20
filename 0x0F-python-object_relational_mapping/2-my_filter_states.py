9a1d6016ce661d23a7fca2c8cd17ca60b32d49f6
GerryNwaribe <the.star.is.geraldine@gmail.com>
2024-02-20 20:54:35 +0100

    made file executable

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,50 +1,27 @@
 #!/usr/bin/python3
-"""Python_ SQL code"""
-
-import sys
+"""Import sys and MySQLdb."""
 import MySQLdb
+import sys
 
-def search_states(username, password, database, state_name):
-    try:
-        # Connect to MySQL server
-        connection = MySQLdb.connect(
-                host='localhost',
-                user=username,
-                passwd=password,
-                db=database,
-                port=3306
-                )
-        cursor = connection.cursor()
+"""Script that takes in an argument and displays all values in the states
+    table of hbtn_0e_0_usa where name matches the argument"""
+if __name__ == "__main__":
+    """Connect to the MySQL database and list all states."""
+    username, password, database_name, state_name_searched = sys.argv[1:5]
 
-        # Execute query to retrieve states matching the state_name
-        cursor.execute("SELECT * FROM states WHERE name LIKE BINARY '{}' "
-                       "ORDER BY id ASC".format(state_name))
+    db = MySQLdb.connect(host="localhost", port=3306, user=username,
+                         passwd=password, db=database_name)
+    cursor = db.cursor()
 
-        states = cursor.fetchall()
+    """Use format to create the SQL query with the user input"""
+    cursor.execute("SELECT * FROM states WHERE name = '{}' "
+                   "ORDER BY states.id ASC".format(state_name_searched))
+    states = cursor.fetchall()
 
-        # Display states
+    if len(states) == 0:
+        print("No records found.")
+    else:
         for state in states:
             print(state)
 
-    except MySQLdb.Error as e:
-        print("MySQL Error {}: {}".format(e.args[0], e.args[1]))
-        sys.exit(1)
-
-    finally:
-        # Close connection
-        if connection:
-            connection.close()
-
-
-if __name__ == "__main__":
-    if len(sys.argv) != 5:
-        print("Usage: python script.py <username> "
-              "<password> <database> <state_name>")
-        sys.exit(1)
-
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-    state_name = sys.argv[4]
-
-    search_states(username, password, database, state_name)
+    db.close()
