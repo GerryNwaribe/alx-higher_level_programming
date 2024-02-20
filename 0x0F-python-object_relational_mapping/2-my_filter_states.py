008b52b4410ebe9a38a597eb7ea66a29e3e74872
GerryNwaribe <the.star.is.geraldine@gmail.com>
2024-02-20 20:32:13 +0100

    4 to 5

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -7,7 +7,7 @@ import sys
     table of hbtn_0e_0_usa where name matches the argument"""
 if __name__ == "__main__":
     """Connect to the MySQL database and list all states."""
-    username, password, database_name, state_name_searched = sys.argv[1:4]
+    username, password, database_name, state_name_searched = sys.argv[1:5]
 
     db = MySQLdb.connect(host="localhost", port=3306, user=username,
                          passwd=password, db=database_name)
