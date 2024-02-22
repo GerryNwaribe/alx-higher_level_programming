d6940f559d8a911f01c7228390debe6b2d9161fe
GerryNwaribe <the.star.is.geraldine@gmail.com>
2024-02-22 01:21:27 +0100

    put sql in variable

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -7,18 +7,20 @@ import sys
     all cities of that state, using the database hbtn_0e_4_usa"""
 if __name__ == "__main__":
     """Connect to the MySQL database and list all states."""
-    username, password, database_name = sys.argv[1:4]
+    username, password, database_name, state_name_searched = sys.argv[1:4]
 
     db = MySQLdb.connect(host="localhost", port=3306, user=username,
                          passwd=password, db=database_name)
     cursor = db.cursor()
 
-    cursor.execute("""SELECT cities.id, cities.name, states.name FROM
-            cities INNER JOIN states ON states.id=cities.state_id""")
-
+    query = """SELECT cities.name FROM cities
+               INNER JOIN states ON states.id=cities.state_id
+               WHERE states.name=%s"""
+    cursor.execute(query, (state_name_searched,))
     rows = cursor.fetchall()
 
-    for row in rows:
-        print(row)
+    city_names = [row[0] for row in rows]
+
+    print(*city_names, sep=", ")
 
     db.close()
