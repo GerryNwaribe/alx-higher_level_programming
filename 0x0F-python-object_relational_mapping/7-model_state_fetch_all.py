c3fd81c8ea62717b9d0f9ff7d84aaf77a1789726
GerryNwaribe <the.star.is.geraldine@gmail.com>
2024-03-18 11:31:43 +0100

    trying differnt documentation style

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-"""Import sys and create_engine.
-Import sessionmaker and Base and State"""
+"""
+script that lists all State objects
+from the database hbtn_0e_6_usa
+"""
 
 import sys
 from sqlalchemy import create_engine
@@ -8,22 +10,33 @@ from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
 if __name__ == "__main__":
-    """Database connection parameters"""
-    username, password, db_name = sys.argv[1], sys.argv[2], sys.argv[3]
-    db_connection = ('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                     .format(username, password, db_name))
+    # Check if the correct number of arguments is provided
+    if len(sys.argv) != 4:
+        print("Usage: python script.py <username> <password> <database>")
+        sys.exit(1)
 
-    """Create engine and bind it to the Base class"""
-    engine = create_engine(db_connection)
+    # Extract MySQL connection information from command-line arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
 
-    """Create a session to interact with the database"""
+    # Create engine to connect to MySQL server
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(username, password, database))
+    # Base.metadata.create_all(engine)
+
+    # Create session maker
     Session = sessionmaker(bind=engine)
+
+    # Create session
     session = Session()
 
-    """Query all State objects and print them"""
+    # Query and list all State objects
     states = session.query(State).order_by(State.id).all()
-    for state in states:
-        print("{}: {}".format(state.id, state.name))
 
-    """Close the session"""
+    # Display the results with specific formatting
+    for state in states:
+        print(state.id, state.name, sep=": ")
+
+    # Close session
     session.close()
