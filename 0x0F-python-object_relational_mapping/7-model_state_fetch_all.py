0d32f7b57219a15b5c4f6a18033eaca19a3cc432
GerryNwaribe <the.star.is.geraldine@gmail.com>
2024-03-18 11:07:00 +0100

    more documentation

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-"""script that lists all State
-objects from the database hbtn_0e_6_usa"""
+"""Import sys and MySQLdb."""
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
+"""script that lists all State
+objects from the database hbtn_0e_6_usa"""
 if __name__ == "__main__":
     """Database connection parameters"""
     username, password, db_name = sys.argv[1], sys.argv[2], sys.argv[3]
