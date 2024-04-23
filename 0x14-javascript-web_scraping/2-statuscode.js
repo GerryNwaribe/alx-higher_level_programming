aacd4f13204a3a5c3754ffe56ef7077a88959409
GerryNwaribe <the.star.is.geraldine@gmail.com>
2024-04-24 00:15:35 +0100

    script for get request

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,2 +1,18 @@
 #!/usr/bin/node
-// script that display the status code of a GET request
+// script that displays the status code of a GET request
+
+const request = require('request');
+
+// Retrieve the URL from the command line arguments
+const url = process.argv[2];
+
+// Make a GET request to the specified URL
+request.get(url, (error, response) => {
+  if (error) {
+    console.error(error);
+    return;
+  }
+
+  // Display the status code
+  console.log(`code: ${response.statusCode}`);
+});
